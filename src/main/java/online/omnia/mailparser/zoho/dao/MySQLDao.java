5d43651553499684631c8051b903c210801a5b43
Vladislav <vladislav.krets@gmail.com>
2017-08-09 18:09:16 +0300

    added MySQLDao interface

--- src/main/java/online/omnia/mailparser/zoho/dao/MySQLDao.java
+++ src/main/java/online/omnia/mailparser/zoho/dao/MySQLDao.java
@@ -1,8 +1,17 @@
 package online.omnia.mailparser.zoho.dao;
 
+import online.omnia.mailparser.zoho.daoentities.EmailAccessEntity;
+import online.omnia.mailparser.zoho.daoentities.EmailSuccessEntity;
+
+import java.util.List;
+
 /**
  * Created by lollipop on 08.08.2017.
  */
 public interface MySQLDao {
-
+    List<EmailAccessEntity> getMailsByCheck(int check);
+    EmailSuccessEntity getEmailSuccesByMessageId(String messageId);
+    void addNewEmailSuccess(EmailSuccessEntity emailSuccessEntity);
+    List<EmailSuccessEntity> getEmailSuccess(int firstSuccessValue, int secondSuccessValue);
+    void updateSuccessEmtity(EmailSuccessEntity emailSuccessEntity);
 }
